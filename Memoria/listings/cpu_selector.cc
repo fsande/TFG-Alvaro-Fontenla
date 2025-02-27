6549eed9cc3df7aee97531c5f5dffaf3b629580f
fsande <fsande@ull.es>
2025-02-27 10:06:04 +0000

    Initial commit Memoria

--- Memoria/listings/cpu_selector.cc
+++
@@ -1,9 +0,0 @@
-#include <sycl/sycl.hpp>
-#include <iostream>
-
-int main() {
-  sycl::queue queue{sycl::cpu_selector_v};
-  std::cout << "Device: "
-            << queue.get_device().get_info<sycl::info::device::name>()
-            << std::endl;
-}
